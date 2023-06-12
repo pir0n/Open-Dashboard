db88db1c690268a57ee9ba561639dcbbf3897be6
pir0n <piron98@gmail.com>
2023-06-12 18:37:08 +0200

    Added basic project directory structure

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+c
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s << endl;
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s << endl;
+    return 0;
+}
